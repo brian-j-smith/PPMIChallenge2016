beb47c730b532ecf10abbde2da794c9acd9873cd
Brian J Smith <brian-j-smith@uiowa.edu>
2016-06-28 20:31:01 -0500

    Remove setwd call from the R project setup script.

--- Programs/Project.R
+++ Programs/Project.R
@@ -1,5 +1,10 @@
-## Set up working directory and environment
-setwd("H:\\Projects\\PPMI")
+## NOTE: The R programs in this project require that the working directory be
+## NOTE: set to the root directory in which the repository is cloned.  Set the
+## NOTE: working directory apart from this file or the other programs prior to
+## NOTE: running them.
+
+
+## Remove all objects from the working environment
 remove(list=objects())
 
 
