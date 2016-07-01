ac5cc41dedf4135654e7757e69ef522999f92887
Stephanie Lussier <stephanie-lussier@uiowa.edu>
2016-07-01 14:18:26 -0500

    create imaging dataset

---
+++ Programs/Data/Imaging.R
@@ -0,0 +1,11 @@
+## Imaging
+str(ind_av133_metadata) #38 variables, information about image being taken
+str(AVIMAG) #32 variables, 
+str(ind_av133_sbr) 
+str(DATSCAN) 
+str(ind_spect_sbr) # 6 variables, caudate and putamen volume?
+str(ind_datscan_source_data) 
+str(ind_mri_source_data) 
+str(MRI) 
+
+
