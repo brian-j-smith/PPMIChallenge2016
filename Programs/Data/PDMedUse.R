8c3aea7c4d173d5e187076c8b2ba04245e039cc2
Brian J Smith <brian-j-smith@uiowa.edu>
2016-08-02 09:20:40 -0500

    Create permanent PD medication use dataset.

---
+++ Programs/Data/PDMedUse.R
@@ -0,0 +1,23 @@
+## PD medication use
+PDMedUse <- join.ppmi(
+  mutate(NUPDRS3, pd_med_any = factor(pmin(pd_med_use, 1), 0:1)),
+  subset = pag_name == "NUPDRS3",
+  select = c(patno, event_id, pd_med_use, pd_med_any),
+  ST2V = TRUE
+)
+
+PDMedUseV <- reshape(
+  PDMedUse,
+  idvar = "patno",
+  timevar = "event_id",
+  direction = "wide"
+)
+
+
+## Summary statistics
+str(PDMedUseV)
+summary(PDMedUseV)
+
+
+## Save datasets
+save(PDMedUse, MotorV, file="Data/PDMedUse.RData")
