ba3112263b0d99adef302cf4f6b365d60d28a51a
Stephanie Lussier <stephanie-lussier@uiowa.edu>
2016-08-30 09:35:28 -0500

    add Imaging Outcomes to Shiny

--- Programs/Analysis/ShinyOutcomes.R
+++ Programs/Analysis/ShinyOutcomes.R
@@ -1,11 +1,14 @@
 load("Programs/Analysis/MotorUPDRS.RData")
+load("Programs/Analysis/Imaging.RData")
 
 OutcomeVars <- c(
-  MotorUPDRSVars
+  MotorUPDRSVars,
+  ImagingVars
 )
 
 OutcomeVals <- c(
-  MotorUPDRSVals
+  MotorUPDRSVals, 
+  ImagingVals
 )
 
 save(VERSION, OutcomeVars, OutcomeVals, file="shiny/Outcomes.RData")
